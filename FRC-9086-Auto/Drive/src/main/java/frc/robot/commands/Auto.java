3d801b22ca7201eb3945d51467c0a569d15074f3
Garrett Pearson <gmpearson2@gmail.com>
2026-03-27 14:03:22 -0400

    more organizing

--- FRC-9086-Auto/Drive/src/main/java/frc/robot/commands/Auto.java
+++ FRC-9086-Auto/Drive/src/main/java/frc/robot/commands/Auto.java
@@ -11,12 +11,16 @@ public class Auto extends SequentialCommandGroup {
     public Auto(DriveSubsystem drive, ShooterSubsystem shooter) {
         addCommands(
 
-            //new InstantCommand(() -> SmartDashboard.putString("Auto Status", "1. Driving Forward")),
-            //new RunCommand(() -> {
-            //    System.out.println("<Drive:> Running Auto.");
-            //    //changed true to false to bypass the gyro system in order to not auto-aim at target
-            //    drive.drive(0.5, 0.0, 0.0, false);
-            //}, drive).withTimeout(2.0),
+            /*
+            Drive forward code. Disabled to only shoot.
+
+            new InstantCommand(() -> SmartDashboard.putString("Auto Status", "1. Driving Forward")),
+            new RunCommand(() -> {
+                System.out.println("<Drive:> Running Auto.");
+                //changed true to false to bypass the gyro system in order to not auto-aim at target
+                drive.drive(0.5, 0.0, 0.0, false);
+            }, drive).withTimeout(2.0),
+            */
 
             new InstantCommand(() -> SmartDashboard.putString("Auto Status", "2. Shooting System Initiated")),
             new RunCommand(() -> shooter.startShootingSystem(1.0), shooter).withTimeout(18.0),
