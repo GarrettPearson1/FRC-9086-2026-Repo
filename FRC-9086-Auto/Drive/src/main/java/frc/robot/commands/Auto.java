4e73ee11e6079cdf4c3cf43116910892a3eb0dd8
Garrett Pearson <gmpearson2@gmail.com>
2026-03-30 16:14:10 -0400

    Refactor Auto command sequence and adjust timeouts

--- FRC-9086-Auto/Drive/src/main/java/frc/robot/commands/Auto.java
+++ FRC-9086-Auto/Drive/src/main/java/frc/robot/commands/Auto.java
@@ -14,17 +14,17 @@ public class Auto extends SequentialCommandGroup {
     public Auto(DriveSubsystem drive, ShooterSubsystem shooter, IntakeSubsystem intake) {
         addCommands(
 
-            /*
-            System.out.println("Initiating Drive");
-            new RunCommand(() -> drive.drive(-0.5, 0.0, 0.0, true), drive).withTimeout(1.0),
-            new RunCommand(() -> drive.drive(0.0, 0.0, 0.0, true), drive)
-            */
+            
+            //System.out.println("Initiating Drive"),
+            new RunCommand(() -> drive.drive(-0.5, 0.0, 0.0, true), drive).withTimeout(2.5),
+            new RunCommand(() -> drive.drive(0.0, 0.0, 0.0, true), drive).withTimeout(0.5),
+            
 
-            System.out.println("Initiating Shooting System");
+            //System.out.println("Initiating Shooting System");
             new RunCommand(() -> shooter.startShootingSystem(0.7), shooter).withTimeout(1.5),
             new RunCommand(() -> shooter.pullMotor(0.9), shooter).withTimeout(4.0),
-            System.out.println("Shutting Down Shooting Motors");
-            new RunCommand(() -> drive.drive(0.0, 0.0, 0.1, true), drive)
+            //System.out.println("Shutting Down Shooting Motors");
+            new RunCommand(() -> drive.drive(0.0, 0.0, 0.1, true), drive),
 
             new InstantCommand(() -> {
                 shooter.stopShootingSystem();
