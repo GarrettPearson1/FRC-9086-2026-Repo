13d8623cf193fa0f9b112d799a27e51f1d6de02a
Garrett Pearson <gmpearson2@gmail.com>
2026-03-31 07:27:38 -0400

    made motor stop

--- FRC-9086-Auto/Drive/src/main/java/frc/robot/commands/Auto.java
+++ FRC-9086-Auto/Drive/src/main/java/frc/robot/commands/Auto.java
@@ -33,21 +33,27 @@ public class Auto extends SequentialCommandGroup {
            new runCommand(() -> {
                 shooter.stopShootingSystem();
                 shooter.stopPull();
-                System.out.println("Auto Complete");
+                System.out.println("Shooting Complete");
             }, shooter).withTimeout(0.3),
 
+            new ClimbMovement(climb, true).withTimeout(1.5),
+
             //Find exactly how long turning 90 degrees takes
-            new RunCommand(() -> drive.drive(0.0, 0.0, 0.1, true), drive).withTimeout(1.5),
+            new RunCommand(() -> drive.drive(0.0, 0.0, -0.5, true), drive).withTimeout(1.5),
+            new RunCommand(() -> drive.drive(0.0, 0.0, 0.0, true), drive).withTimeout(0.1),
+
 
             //Sets Up climber
             new ClimbMovement(climb, true).withTimeout(1.0),
 
             //Drives to Post
             new RunCommand(() -> drive.drive(0.0, -0.5, 0.0, true), drive).withTimeout(1.0),
-            new RunCommand(() -> drive.drive(0.0, 0.0, 0.0, true), drive).withTimeout(0.5),
+            new RunCommand(() -> drive.drive(0.0, 0.0, 0.0, true), drive).withTimeout(0.1),
 
             //Climbing the Ladder
             new ClimbMovement(climb, false).withTimeout(3.0),
+            
+            
 
         );
     }
